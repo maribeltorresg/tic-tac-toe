52404577816d873123ea0211a8fb2716c56a57a9
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-11-13 01:55:36 -0500

    Agregando el evento click

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 // cuando esta totalmente cargado
 window.onload = function() {
-  var board = document.querySelector('.board-js');
+  //
+  var tds = Array.prototype.slice.call(document.getElementsByTagName('td'));
+  var symbol = 'x';
 
-  board.addEventListener('click', addX);
+  // es como recorrer con for 
+  tds.forEach(function(td, index) {
+    td.addEventListener('click', function click() {// console.log('diste click en cuadrado: '+ index);
+      td.removeEventListener('click', click);
+      
+      td.innerHTML = symbol;
+      if (symbol === 'x') {
+        symbol = 'o';
+      } else {
+        symbol = 'x';
+      }
+
+    });
+  });
+
+
+
 };
-var centinel = true;
-function addX(event) {
-  if (event.target.matches('td') && event.target.textContent === '') {
-    if (centinel) { 
-      event.target.textContent = 'x';
-    } else { 
-      event.target.textContent = '0';
-    }
-    centinel = !centinel;
-  }
-}
